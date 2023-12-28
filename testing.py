7c300fe225b4807ed5e81ca273ce489323f0425c
zeke <zekejohn118@gmail.com>
2023-12-28 15:04:33 -0800

    initital commint

---
+++ testing.py
@@ -0,0 +1,22 @@
+
+# !python3 -m pip install -U git+https://github.com/facebookresearch/audiocraft#egg=audiocraft
+
+# !pip install wandb pydu
+
+# !export PYTHONIOENCODING=utf-8
+
+from audiocraft.models import musicgen
+from audiocraft.utils.notebook import display_audio
+import torch
+
+model = musicgen.MusicGen.get_pretrained('medium', device='cuda')
+
+model.set_generation_params(duration=30)
+
+model.lm.load_state_dict(torch.load('models/lm_final.pt'))
+
+res = model.generate([
+    'drake type rap beat'
+],
+    progress=True)
+display_audio(res, 32000)
